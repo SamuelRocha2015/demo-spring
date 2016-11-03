80ec6345590f1ea807f7fcc83f32c72c811950d0
Samuel Rocha <samuelrodriguessr@hotmail.com>
2016-11-03 14:27:41 -0300

    Boot do projeto.

---
+++ src/main/java/br/com/codeshouse/spring/conf/AppWebConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.codeshouse.spring.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.codeshouse.spring.controller.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class})
+public class AppWebConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver (){
+		InternalResourceViewResolver resolve = new InternalResourceViewResolver();
+		resolve.setPrefix("/WEB-INF/views/");
+        resolve.setSuffix(".jsp");
+        return resolve;
+	}
+	
+}
